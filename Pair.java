f0cd4be3228c886ad96286497901406101bc8052
Ender <Ender@192.168.77.157>
2023-03-19 21:30:10 -0500

    Created abstract class Pair
    Pair is the Parent of DataPair and ValuePair

---
+++ Pair.java
@@ -0,0 +1,22 @@
+// Abstract class with children DataPair and ValuePair, to get rid of warnings
+public abstract class Pair<T extends Object> {
+
+	private String name;
+	private T data;
+	
+	public Pair(String name, T data) {
+		this.name = name;
+		this.data = data;
+	}
+	
+	public String getName() { return this.name; }
+	public T getData() { return this.data; }
+	
+	public String toString() { return "(" + this.getName() + ", " + this.getData() + ")"; }
+	
+	@Override
+	public int hashCode() {
+		return this.getName().hashCode() * this.getData().hashCode();
+	}
+	
+}
