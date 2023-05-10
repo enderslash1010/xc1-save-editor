9fdc1ae1b0c7e1ef71428de9d855564f31d0b80b
ender <ender@10.0.0.118>
2023-05-09 20:07:30 -0500

    Created ModelEvent and ViewEvent
    These are created by the model and view, respectfully, to signal an
    event to the controller to act upon

---
+++ ModelEvent.java
@@ -0,0 +1,31 @@
+import java.util.EventObject;
+
+/**
+ *	class ModelEvent
+ *
+ *	represents an event that the model can broadcast to controller
+ */
+public class ModelEvent extends EventObject {
+	
+	private int type;
+	private String param;
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static final int SET_DATA = 0; // param contains name of data changed
+
+	public ModelEvent(Object source, int type, String param) {
+		super(source);
+		this.type = type;
+		this.param = param;
+	}
+	
+	public ModelEvent(Object source, int type) {
+		super(source);
+		this.type = type;
+	}
+	
+	public int getType() { return this.type; }
+	public String getParam() { return this.param; }
+	
+}
---
+++ ViewEvent.java
@@ -0,0 +1,44 @@
+import java.util.EventObject;
+
+/**
+ *	class ViewEvent
+ *
+ *	represents an event that the view can broadcast to controller
+ */
+public class ViewEvent extends EventObject {
+	
+	private static final long serialVersionUID = 1L;
+	
+	// describes type of ViewEvent, using one of the defined constants
+	private int type;
+	
+	// contains passed data from the View that the Controller needs to handle the specific event
+	private String param;
+	
+	/*
+	 *   Constants
+	 */
+	
+	public static final int OPEN_FILE = 0; // param contains file location of selected file to be opened
+
+	public static final int SAVE_FILE = 1; // param null
+	
+	public static final int GET_DATA = 2; // param contains name of data to get
+
+	public static final int SET_DATA = 3; // param is in form "dataName:dataValue" to set dataName to dataValue
+
+	public ViewEvent(Object source, int type, String param) {
+		super(source);
+		this.type = type;
+		this.param = param;
+	}
+	
+	public ViewEvent(Object source, int type) {
+		super(source);
+		this.type = type;
+	}
+	
+	public int getType() { return this.type; }
+	public String getParam() { return this.param; }
+	
+}
