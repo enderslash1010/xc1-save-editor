efb7b74ec0f239a13a9ba2b009faf90f04719cf7
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-07-07 09:30:19 -0500

    Created ArrayField and SaveField enums
    These enums serve the same purpose as a String name when refering to a
    save field or array column, but is easier to code with/looks cleaner

---
+++ Controller/ArrayField.java
@@ -0,0 +1,15 @@
+package Controller;
+
+/**
+ * Enum for array column names
+ * Used to attach a common name to an <code>Element</code> in an <code>Array</code>
+ * @author ender
+ */
+public enum ArrayField {
+	// MINE
+	mineCooldown, numHarvests, minelistID, mapID,
+	
+	// TBOX
+	blank, xBox, yBox, zBox, boxAngle, boxRank, boxDropTable, boxMapID,
+	
+}
